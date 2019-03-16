b36c2494f3f9064f77f46893f6b68e6422cc05e8
wick7 <wickersham.craig@gmail.com>
2019-03-15 18:26:27 -0700

    update

---
+++ config/key.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    stripe: {
+        keySecret: 'sk_test_eELjza4IZKEvYTrjKbqaxsmU'
+    },
+    session: {
+        cookieKey: 'mypassportkey'
+    },
+    mongoL: {
+        url: 'mongodb://ctw:password1@ds049744.mlab.com:49744/heroku_jhpb9hnt'
+    }
+};
