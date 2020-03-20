774278e77c6348653059e244481d0ab73d715344
Zachary <zdadams1@gmail.com>
2020-03-20 15:51:38 -0700

    fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -13,18 +13,12 @@ export default class Footer extends Component {
         <div className='container'>
           <div className='row row-pb-md items'>
             <div className='col-md-3 colorlib-widget'>
-              <h4>About Me</h4>
+              <h4>About</h4>
               <p>
                 I make computers and software to help people with their lives.
               </p>
               <p></p>
-              <ul className='colorlib-social-icons'>
-                <li>
-                  <a href='#'>
-                    <i className='icon-facebook' />
-                  </a>
-                </li>
-              </ul>
+              <ul className='colorlib-social-icons'></ul>
               <p />
             </div>
             <div className='col-md-2 colorlib-widget'>
@@ -38,7 +32,7 @@ export default class Footer extends Component {
               <p />
             </div>
             <div className='col-md-3'>
-              <h4>Contact Information</h4>
+              <h4>Contact</h4>
               <ul className='colorlib-footer-links'>
                 <li>
                   <a href='tel://1234567920'>+1 209 202 4527</a>
