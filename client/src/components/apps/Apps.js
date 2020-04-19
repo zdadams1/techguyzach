4fa4cb970cc6746380d13d83d4fd12576325c3d7
Zachary <zdadams1@gmail.com>
2020-04-19 03:13:07 -0700

    'css'

--- client/src/components/apps/Apps.js
+++ client/src/components/apps/Apps.js
@@ -7,45 +7,48 @@ import './apps.css';
 export default class Apps extends Component {
   render() {
     return (
-      <div className='apps container'>
+      <div>
         <Nav />
-        <div className='card apps-header'>
-          <div className='card-body'>
-            <h1>Software</h1>
-            <p>My custom built Applications.</p>
-          </div>
-        </div>
-        <ul className='apps'>
-          <li>
-            <img src={jtlogo} />
-          </li>
-          <li>
-            <h1>JobTrade</h1>
-          </li>
-          <li>
-            <p>Buy, Sell, Work and chat with people in your area.</p>
-          </li>
-          <li>
-            <img src={blockstarlogo} />
-          </li>
-          <li>
-            <h1>Blockstar</h1>
-          </li>
-          <li>
-            <p>Earn points for lining up 4 colors in a row.</p>
-          </li>
-          <li>
-            {' '}
-            <div className='card apps-donate'>
-              <div className='card-body'>
-                <h1>Coming Soon!</h1>
-                <a href='https://paypal.me/hypenac' className='donate-btn'>
-                  Donate
-                </a>
-              </div>
+
+        <div className='apps container'>
+          <div className='card apps-header'>
+            <div className='card-body'>
+              <h1>Software</h1>
+              <p>My custom built Applications.</p>
             </div>
-          </li>
-        </ul>
+          </div>
+          <ul className='apps'>
+            <li>
+              <img src={jtlogo} />
+            </li>
+            <li>
+              <h1>JobTrade</h1>
+            </li>
+            <li>
+              <p>Buy, Sell, Work and chat with people in your area.</p>
+            </li>
+            <li>
+              <img src={blockstarlogo} />
+            </li>
+            <li>
+              <h1>Blockstar</h1>
+            </li>
+            <li>
+              <p>Earn points for lining up 4 colors in a row.</p>
+            </li>
+            <li>
+              {' '}
+              <div className='card apps-donate'>
+                <div className='card-body'>
+                  <h1>Coming Soon!</h1>
+                  <a href='https://paypal.me/hypenac' className='donate-btn'>
+                    Donate
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
