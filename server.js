614f8839ea96c4c6f507a183068b4c291a3972d9
Zachary <zdadams1@gmail.com>
2020-06-04 19:01:58 -0700

    'debugging'

--- server.js
+++ server.js
@@ -2,24 +2,33 @@ const express = require('express');
 var cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const app = express();
 var bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 const http = require('http');
-const app = express();
 var session = require('express-session');
 const message = require('./routes/api/message');
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 var MongoStore = require('connect-mongo')(session);
-const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets - Connects React to Backend -
+if (process.env.NODE_ENV === 'production') {
+  //  app.use(express.static("client/public"));
+  app.use(express.static('client/build'));
+}
+
+// Sessions
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const db = require('./config/keys');
-console.log(typeof db);
-
 // Connect to MongoDB
 mongoose
   .createConnection(
@@ -60,36 +69,15 @@ options.secretOrKey = keys.secretOrKey;
   );
 };
 
-// Sessions
-
-// app.use(cookieParser());
-// app.use(
-//   session({
-//     secret: 'mysupersecret',
-//     resave: false,
-//     saveUninitialized: true,
-//     httpOnly: false,
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//     cookie: { maxAge: 180 * 60 * 1000 },
-//   })
-// );
-
 app.use(routes);
 
-// app.use(function (req, res, next) {
-//   res.locals.login = req.isAuthenticated();
-//   res.locals.session = req.session;
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  next();
+});
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-const PORT = process.env.PORT || 3001;
-server.listen(PORT);
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
